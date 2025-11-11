faac461d777e194f06b405010c37805e9e0d6ba3
Andrey Kalmatskiy <andrewkoder82@gmail.com>
2025-11-11 15:23:15 -0500

    switch to builtin depends

--- build-win.bat
+++ build-win.bat
@@ -1,42 +1,15 @@
 @echo on
 setlocal enabledelayedexpansion
 
+set triplet=%1
 set GENERATOR=Ninja
-
 set CONFIGS=Release Debug
 
 if exist "update.bat" (
     call update.bat
 )
 
-set VS_VCVARSALL="C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat"
-
 for /F %%p in ('powershell -Command "(Get-CimInstance Win32_Processor).NumberOfLogicalCores"') do set CORES=%%p
-
-REM --------------------------------
-REM Compile platforms
-REM --------------------------------
-call :compilePlatform x64         x64-windows
-call :compilePlatform amd64_arm64 arm64-windows
-
-echo Build complete!
-endlocal
-exit /b
-
-
-REM ==============================================
-REM Function: compilePlatform vcvars_arg triplet
-REM ==============================================
-:compilePlatform
-setlocal
-set build_mode=%1
-set triplet=%2
-
-echo ================================
-echo Building for %triplet% (%build_mode%)
-echo ================================
-
-call %VS_VCVARSALL% %build_mode%
 
 set BUILD_DIR=build\%triplet%
 if not exist "!BUILD_DIR!" mkdir "!BUILD_DIR!"
@@ -54,6 +27,3 @@ for %%c in (%CONFIGS%) do (
 
     cmake --build "!BUILD_DIR!" --parallel !CORES!
 )
-
-endlocal
-exit /b
