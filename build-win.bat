74fdfcac9d114b93f16df4e312094439d17690fb
Andrey Kalmatskiy <andrewkoder82@gmail.com>
2025-11-11 16:09:48 -0500

    replace !VARS!  -> %VARS% to see correct echo

--- build-win.bat
+++ build-win.bat
@@ -12,18 +12,18 @@ if exist "update.bat" (
 for /F %%p in ('powershell -Command "(Get-CimInstance Win32_Processor).NumberOfLogicalCores"') do set CORES=%%p
 
 set BUILD_DIR=build\%triplet%
-if not exist "!BUILD_DIR!" mkdir "!BUILD_DIR!"
+if not exist "%BUILD_DIR%" mkdir "%BUILD_DIR%"
 set OUT_DIR=..\..\out
-if not exist "!OUT_DIR!" mkdir "!OUT_DIR!"
+if not exist "%OUT_DIR%" mkdir "%OUT_DIR%"
 for %%c in (%CONFIGS%) do (
-    echo Building %triplet%, config %%c in !BUILD_DIR!
+    echo Building %triplet%, config %%c in %BUILD_DIR%
 
-    cmake -S "." -B "!BUILD_DIR!" -G "%GENERATOR%" ^
+    cmake -S "." -B "%BUILD_DIR%" -G "%GENERATOR%" ^
         -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" ^
         -DVCPKG_TARGET_TRIPLET=%triplet% ^
         -DCMAKE_BUILD_TYPE=%%c ^
-        -DAG_OUT_DIR=!OUT_DIR! ^
+        -DAG_OUT_DIR=%OUT_DIR% ^
         -DAG_TRIPLE=%triplet%
 
-    cmake --build "!BUILD_DIR!" --parallel !CORES!
+    cmake --build "%BUILD_DIR%" --parallel %CORES%
 )
